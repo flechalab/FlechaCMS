cc3bbb1f4f753b216a97c52a91803ea656803271
Fernando Dias <fdias.webdev@gmail.com>
2011-07-27 20:19:26 -0300

    Gitignore updated

--- application/config/database.php
+++ application/config/database.php
@@ -37,10 +37,16 @@
 $active_group = "default";
 $active_record = TRUE;
 
+//$db['default']['hostname'] = "localhost";
+//$db['default']['username'] = "root";
+//$db['default']['password'] = "root";
+//$db['default']['database'] = "flecha-site";
+
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "root";
-$db['default']['password'] = "root";
-$db['default']['database'] = "flecha-site";
+$db['default']['username'] = "";
+$db['default']['password'] = "";
+$db['default']['database'] = "";
+
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
